e50e43a873cd42ac84d8fb1455b558c794d32bc0
Cedric Sirianni <cedric@sirianni.dev>
2025-01-29 16:50:46 -0500

    Update vimtex settings

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,8 +1,13 @@
+-- The default local leader key is \. See :help vimtex for more info.
+-- To live preview, \lv.
+-- To start continuous compilation, \ll.
+
 return {
-  "lervag/vimtex",
-  init = function()
-    -- Use init for configuration, don't use the more common "config".
-    vim.g.vimtex_view_method = "skim"
-    vim.g.tex_flavor = 'latex'
-  end
+	"lervag/vimtex",
+	init = function()
+		-- Use init for configuration, don't use the more common "config".
+		vim.g.vimtex_view_method = "skim"
+		vim.g.tex_flavor = "latex"
+		vim.g.vimtex_quickfix_open_on_warning = 0
+	end,
 }
