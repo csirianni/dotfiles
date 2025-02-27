c86d865f65a872ba6b9821428fd7355073946808
Cedric Sirianni <cedric@sirianni.dev>
2025-02-27 17:44:25 -0500

    Add Python linter/formatter

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -57,6 +57,39 @@ return {
 			})
 			lspconfig.pyright.setup({
 				capabilities = capabilities,
+				-- Use Ruff exclusively for linting, formatting, and organizing imports.
+				settings = {
+					pyright = {
+						-- Using Ruff's import organizer
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							-- Ignore all files for analysis to exclusively use Ruff for linting
+							ignore = { "*" },
+						},
+					},
+				},
+			})
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+			})
+
+			-- Defer to pyright for hover
+			-- https://docs.astral.sh/ruff/editors/setup/#neovim
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client == nil then
+						return
+					end
+					if client.name == "ruff" then
+						-- Disable hover in favor of Pyright
+						client.server_capabilities.hoverProvider = false
+					end
+				end,
+				desc = "LSP: Disable hover capability from Ruff",
 			})
 
 			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
