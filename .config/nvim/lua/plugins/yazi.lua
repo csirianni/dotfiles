eccdf0fb4d3197d31a3bfcefce7c3d4ce65e6166
Cedric Sirianni <cedric@sirianni.dev>
2025-04-26 17:03:57 -0400

    Add yazi.lua for nvim

---
+++ .config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,20 @@
+return {
+	"mikavilpas/yazi.nvim",
+	keys = {
+		{
+			"<leader>pv",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+	},
+	init = function()
+		-- Block netrw plugin load
+		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
+	event = "VeryLazy",
+	opts = {
+		open_for_directories = true,
+	},
+}
