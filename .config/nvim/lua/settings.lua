9384570a332cf9f4b5a1eb02508526164fbdcd42
Cedric Sirianni <cedric.sirianni@mongodb.com>
2025-09-24 14:07:46 +0000

    Add textwidth to settings.lua

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -20,7 +20,7 @@ vim.opt.smartindent = true
 -- Wrap lines
 vim.opt.wrap = true
 vim.opt.linebreak = true
-vim.opt.textwidth = 0
+vim.opt.textwidth = 100
 vim.opt.wrapmargin = 0
 -- Set highlight on search, but clear on pressing <Esc> or <C-c> in normal mode
 vim.opt.hlsearch = true
@@ -45,20 +45,20 @@ vim.opt.updatetime = 50
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	pattern = "*",
-	command = "silent! lua vim.highlight.on_yank({ timeout = 500 })",
+    pattern = "*",
+    command = "silent! lua vim.highlight.on_yank({ timeout = 500 })",
 })
 
 -- Jump to last edit position on opening file
 vim.api.nvim_create_autocmd("BufReadPost", {
-	pattern = "*",
-	callback = function()
-		if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
-			-- except for in git commit messages
-			-- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
-			if not vim.fn.expand("%:p"):find(".git", 1, true) then
-				vim.cmd('exe "normal! g\'\\""')
-			end
-		end
-	end,
+    pattern = "*",
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            -- except for in git commit messages
+            -- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
+            if not vim.fn.expand("%:p"):find(".git", 1, true) then
+                vim.cmd('exe "normal! g\'\\""')
+            end
+        end
+    end,
 })
