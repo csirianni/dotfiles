f502c0ae27550cfebf8df24f954c41655d671577
Cedric Sirianni <cedric.sirianni@mongodb.com>
2025-10-22 14:29:30 +0000

    Add treesitter-context plugin

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,18 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				auto_install = true,
-				ignore_install = { "latex" },
-				highlight = { enable = true, disable = { "latex" } },
-				indent = { enable = true },
-			})
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                auto_install = true,
+                ignore_install = { "latex" },
+                highlight = { enable = true, disable = { "latex" } },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+    }
 }
