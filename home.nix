13e9708122ab897410d9643b812a083ad90de9cf
Cedric Sirianni <cedric.sirianni@mongodb.com>
2025-12-02 20:55:43 +0000

    Add packages to home.nix

--- home.nix
+++ home.nix
@@ -8,6 +8,23 @@
   
   home.stateVersion = "26.05";
 
+  home.packages = with pkgs; [
+    neovim
+    tmux
+    yazi
+    zsh
+    zsh-syntax-highlighting
+    zsh-autosuggestions
+    nodejs
+    ripgrep
+
+    llvm
+    clang-tools
+    nodePackages.prettier
+    pyright
+    ruff
+  ];
+
   home.file = {
     ".tmux.conf".source = ./.config/tmux/.tmux.conf;
     ".zshrc".source = ./.config/zsh/.zshrc;
