0d08fa5d021cdb9dacc5da8d9b72c34acbcb7cfc
Cedric Sirianni <cedric.sirianni@mongodb.com>
2025-12-02 20:56:03 +0000

    Replace zsh/.zshrc with programs.zsh

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, username, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.home-manager.enable = true;
@@ -27,9 +27,50 @@
 
   home.file = {
     ".tmux.conf".source = ./.config/tmux/.tmux.conf;
-    ".zshrc".source = ./.config/zsh/.zshrc;
     ".config/ghostty".source = ./.config/ghostty;
     ".config/nvim".source = ./.config/nvim;
     ".config/yazi".source = ./.config/yazi;
   };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [ "git" ];
+    };
+
+    envExtra = ''
+      export EDITOR="nvim"
+      export TERM="xterm-256color"
+    '';
+
+    shellAliases = {
+      gs = "git status";
+      gl = "git log";
+      gb = "git branch";
+      vim = "nvim";
+      e = "nvim";
+    };
+
+    initContent = ''
+      # yazi function
+      y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          builtin cd -- "$cwd"
+        fi
+        rm -f -- "$tmp"
+      }
+
+      ssh-add --apple-use-keychain ~/.ssh/id_ed25519 2>/dev/null || true
+    '';
+  };
+
 }
