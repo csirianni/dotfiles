43a3ad3047c7471c332d1cc3f12861e19c29becd
Cedric Sirianni <cedric.sirianni@mongodb.com>
2025-12-03 16:38:24 +0000

    Remove Mason

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,5 @@
 return {
     {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                -- ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
-                ensure_installed = { "lua_ls", "ts_ls", }
-            })
-        end,
-    },
-    {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
