6d9e22ae12116eea04ead4d8ce6ee5c166dacb4f
Cedric Sirianni <cedric.sirianni@mongodb.com>
2025-12-06 02:04:57 +0000

    Use live_grep_args for <leader>fg

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,10 @@ return {
         "nvim-telescope/telescope-ui-select.nvim",
     },
     {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        version = "^1.0.0",
+    },
+    {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
         dependencies = { "nvim-lua/plenary.nvim" },
@@ -21,10 +25,13 @@ return {
                     },
                 },
             })
+
+            local telescope = require("telescope")
             local builtin = require("telescope.builtin")
+
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
             vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = 'Find Recent Files ("." for repeat)' })
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, {})
             vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
             vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
             vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
@@ -39,7 +46,8 @@ return {
             vim.keymap.set("n", "<C-t>", builtin.treesitter, {})
             vim.keymap.set("n", "gr", builtin.lsp_references, {})
 
-            require("telescope").load_extension("ui-select")
+            telescope.load_extension("ui-select")
+            telescope.load_extension("live_grep_args")
         end,
     },
 }
