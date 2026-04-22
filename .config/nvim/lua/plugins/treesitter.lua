fd941c171e48d586204acfb0a364cf377de0ca86
Cedric Sirianni <cedric.sirianni@mongodb.com>
2026-04-21 20:10:54 -0400

    Use nvim-treesitter highlighting and indentation

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,3 +16,13 @@ vim.api.nvim_create_autocmd("FileType", {
         end
     end,
 })
+
+-- Use treesitter highlighting and indentation
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
